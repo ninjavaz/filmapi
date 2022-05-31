d5ae04c832660fdca6077fd81f9bf426a3077cab
Dominik <dknuszkiewicz@gmail.com>
2022-05-31 20:30:26 +0200

    init commit

---
+++ src/test/java/com/kni/filmapi/FilmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kni.filmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
